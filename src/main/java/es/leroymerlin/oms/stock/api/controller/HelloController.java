ca5d5294671b9c58b54c5bfb38fba94b16f734b2
Óscar <omontes@paradigmadigital.com>
2020-06-23 14:19:21 +0200

    Add CorsFilter

--- src/main/java/es/leroymerlin/oms/stock/api/controller/HelloController.java
+++ src/main/java/es/leroymerlin/oms/stock/api/controller/HelloController.java
@@ -4,7 +4,10 @@ import es.leroymerlin.oms.stock.api.model.Hello;
 import es.leroymerlin.oms.stock.api.service.HelloService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +17,8 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
 
-    @GetMapping("/hello")
+    @RequestMapping(value = "Hello", method = RequestMethod.GET, produces = {
+            MediaType.APPLICATION_JSON_VALUE})
     public Hello sayHello() {
         log.info("Say Hello");
         return helloService.sayHello();
